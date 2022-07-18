ac0ab5d849b95ded4771fe2b10067c991c86aa20
Awusa Collins <awusacollins@gmail.com>
2022-07-18 10:28:06 +0100

    More changes

--- post/models.py
+++ post/models.py
@@ -1,8 +1,12 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 # Create your models here.
 class post(models.Model):
     title = models.CharField(max_length=100)
-    body = models.CharField(max_length=100000)
+    body = models.TextField(max_length=100000)
+    thumbnail = models.ImageField(upload_to = "images/", blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     date_created = models.DateTimeField(datetime.now, blank=True)
 
